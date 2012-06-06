792e1450224a3794dd633df5e2c591973dae4ac6
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2012-06-06 12:51:37 -0300

    Removed .swp file. Fix project url.

--- spent_time_required/init.rb
+++ spent_time_required/init.rb
@@ -11,7 +11,7 @@ Redmine::Plugin.register :spent_time_required do
   author 'Rodolfo Stangherlin'
   description 'Plugin to require adding spent time'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
+  url 'https://github.com/rodolfo3/Redmine-Plugin--Spent-Time-Required/tree/master/spent_time_required'
   author_url 'https://github.com/rodolfo3'
 
   settings(:default => {
